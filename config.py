e487cbd42c4e8cf088f89520f21147eee02e5db9
sharker559 <sharker559@gmail.com>
2025-10-01 23:30:16 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+TOKEN = "1756496815:AAGb9L58RP694i9tFyxe0ATRFFpUr3sB_i8"
+
+WEBHOOK_HOST = "https://yegorchik.pythonanywhere.com"  # замени username
+WEBHOOK_PATH = f"/{TOKEN}"
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
